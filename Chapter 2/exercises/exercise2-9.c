242573390d5bed6d715e94b32a15859393088ad5
Bruno Ladeia <bruno13dm@outlook.com>
2022-04-25 21:58:46 -0300

    Complete subsection 2.11

--- Chapter 2/exercises/exercise2-9.c
+++ Chapter 2/exercises/exercise2-9.c
@@ -1,5 +1,12 @@
 #include <assert.h>
 
+/*
+ * Exercise 2-9.
+ * In a two's complement number system, x &= (x-1) deletes
+ * the rightmost 1-bit in x. Explain why. Use this observation
+ * to write a faster version of bitcount.
+ */
+
 #define NDEBUG
 
 int bitcount(unsigned num);
